62fd39e33fb47cb716f40cf9f1c9d36162468d03
kermakovich <kermakovich.laba@solvd.com>
2023-03-19 10:19:25 +0300

    Webflux is fixed #20

--- src/main/java/solvd/laba/ermakovich/hf/web/WebClientConfig.java
+++ src/main/java/solvd/laba/ermakovich/hf/web/WebClientConfig.java
@@ -1,6 +1,5 @@
 package solvd.laba.ermakovich.hf.web;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,7 +11,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    @LoadBalanced
     public WebClient.Builder webClient() {
         return WebClient.builder();
     }
